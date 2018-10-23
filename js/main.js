5afbf42bae60bb788f583645eef587d2ee4098be
QBS <tadasbytautas1@gmail.com>
2018-10-23 22:44:23 +0100

    more content

--- js/main.js
+++ js/main.js
@@ -24,23 +24,12 @@ $('.stop').on('click', function() {
 })
 
 // Transition effect for navbar
-$(document).ready(function() {
-    $(window).scroll(function() {
-        // checks if window is scrolled more than 100px, adds/removes solid class
-        if ($(this).scrollTop() > 100) {
-            $('.navbar').removeClass('bg-transparent').addClass('bg-light');
-        } else {
-            $('.navbar').addClass('bg-transparent');
-        }
-    });
-});
-
-// Solid color navbar on mobile
-$(document).ready(function() {
-    if ($(window).width() < 992) {
-       $('.navbar').removeClass('bg-transparent').addClass('bg-light');
-    }
-    else {
-       // $('.navbar').addClass('bg-transparent');
-    }
-});
+// $(document).ready(function() {
+//     $(window).scroll(function() {
+//         if ($(this).scrollTop() > 100) {
+//             $('.navbar').removeClass('bg-transparent').addClass('bg-light');
+//         } else {
+//             $('.navbar').addClass('bg-transparent');
+//         }
+//     });
+// });
