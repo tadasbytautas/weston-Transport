e6cc0bd773b0398ba2abe19ab094ac8c2de139da
QBS <tadasbytautas1@gmail.com>
2018-11-01 21:34:34 +0000

    added galery

--- js/main.js
+++ js/main.js
@@ -23,13 +23,32 @@ $('.stop').on('click', function() {
     owl.trigger('stop.owl.autoplay')
 })
 
-// Transition effect for navbar
 // $(document).ready(function() {
-//     $(window).scroll(function() {
-//         if ($(this).scrollTop() > 100) {
-//             $('.navbar').removeClass('bg-transparent').addClass('bg-light');
-//         } else {
-//             $('.navbar').addClass('bg-transparent');
-//         }
-//     });
-// });
+//
+//   var scrollLink = $('.scroll');
+//
+//   // Smooth scrolling
+//   scrollLink.click(function(e) {
+//     e.preventDefault();
+//     $('body,html').animate({
+//       scrollTop: $(this.hash).offset().top
+//     }, 1000 );
+//   });
+//
+//   // Active link switching
+//   $(window).scroll(function() {
+//     var scrollbarLocation = $(this).scrollTop();
+//
+//     scrollLink.each(function() {
+//
+//       var sectionOffset = $(this.hash).offset().top - 20;
+//
+//       if ( sectionOffset <= scrollbarLocation ) {
+//         $(this).parent().addClass('active');
+//         $(this).parent().siblings().removeClass('active');
+//       }
+//     })
+//
+//   })
+//
+// })
