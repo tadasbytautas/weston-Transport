c2b7cb05b293b93937197f8371d35560626ec317
QBS <tadasbytautas1@gmail.com>
2018-11-03 12:35:33 +0000

    smooth scrolling

--- js/main.js
+++ js/main.js
@@ -23,32 +23,29 @@ $('.stop').on('click', function() {
     owl.trigger('stop.owl.autoplay')
 })
 
-// $(document).ready(function() {
-//
-//   var scrollLink = $('.scroll');
-//
-//   // Smooth scrolling
-//   scrollLink.click(function(e) {
-//     e.preventDefault();
-//     $('body,html').animate({
-//       scrollTop: $(this.hash).offset().top
-//     }, 1000 );
-//   });
-//
-//   // Active link switching
-//   $(window).scroll(function() {
-//     var scrollbarLocation = $(this).scrollTop();
-//
-//     scrollLink.each(function() {
-//
-//       var sectionOffset = $(this.hash).offset().top - 20;
-//
-//       if ( sectionOffset <= scrollbarLocation ) {
-//         $(this).parent().addClass('active');
-//         $(this).parent().siblings().removeClass('active');
-//       }
-//     })
-//
-//   })
-//
-// })
+// SMOOTH SCROLL
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 600, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
